a0bafe083b8d7ae37be316cbbe25b0502e1480b1
RenzoElias <renzoeliasdelacalle@gmail.com>
2021-11-01 06:21:40 -0500

    fix all

--- src/app/solicitudes/services/solicitudes.service.ts
+++ src/app/solicitudes/services/solicitudes.service.ts
@@ -18,39 +18,27 @@ export class SolicitudesService {
   getAllSolicitud():Observable<ResponseI> {
     return this.http.get<ResponseI>(`http://192.168.1.4:1991/api/solicitud/requerimientos/obtener-solicitud`);
   }
-
-  // OBTENER TODO TIPO DE SOLICITUD
   getAllTipoSolicitud():Observable<ResponseI> {
     return this.http.get<ResponseI>(`http://192.168.1.4:1991/api/solicitud/parametros/find-all?idParametrosTipo=5`);
   }
-
-  // OBTENER UNA SOLICITUD
   getSolicitudPorId( id: string ):Observable<ResponseI> {
     return this.http.get<ResponseI>(`http://192.168.1.4:1991/api/solicitud/requerimientos/obtener-solicitud?idSolicitud=${ id }`);
   }
-  // OBTENER USER SOLICITUD
   getUserSolicitudPorId( id: number ):Observable<ResponseI> {
     return this.http.get<ResponseI>(`http://192.168.1.4:1991/api/solicitud/persona/obtenerPersona?idPersona=${ id }`);
   }
-
-
-  // AGREGAR UNA SOLICITUD
+  // actualizar
   // agregarSolicitud( solicitud: Solicitud ): Observable<Solicitud> {
   //   return this.http.post<Solicitud>(`${ this.baseUrl }/api`, solicitud );
   // }
 
-  // // ACTUALIZAR
+  // actualizar
   // actualizarSolicitud( solicitud: Solicitud ): Observable<Solicitud> {
   //   return this.http.put<Solicitud>(`${ this.baseUrl }/api/${ solicitud.id }`, solicitud );
   // }
 
-  // BORRAR
   borrarSolicitud( id: string ): Observable<any> {
     return this.http.delete<any>(`${ this.baseUrl }/api/${ id }`);
   }
-
-  // getSugerencias( termino: string ): Observable<Solicitud[]> {
-  //   return this.http.get<Solicitud[]>(`${ this.baseUrl }/api?q=${ termino }&_limit=6`);
-  // }
 
 }
